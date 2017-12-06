2d0b4011aff6d5ab804a27445d02ae85f52eff6d
Ammar Husain <ammo700@gmail.com>
2017-12-06 18:16:36 -0500

    Added base UI

---
+++ routes/party.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// define the home page route
+router.get('/getParties', function(req, res) {
+
+});
+
+
+module.exports = router;
